a038ffc6075b935a1574a9d08a50eb5ffcbe078f
Rik Brutsaert <hb31@cornell.edu>
2018-01-05 14:21:43 -0800

    add get todos id

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ var bodyParser=require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo}=require('./models/todo');
 var {User}=require('./models/user');
-
+const {ObjectID}=require('mongodb');
 var app=express();
 
 app.use(bodyParser.json());
@@ -30,6 +30,21 @@ app.get('/todos',(req,res)=>{
     (e)=>{
       res.status(400).send(e);
     });
+});
+//GET /todos/123214
+
+app.get('/todos/:id',(req,res)=>{
+  var id=req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+  Todo.findById(id).then(
+    (r)=>{
+      if(r){return res.send({r})}
+      res.status(400).send();
+    },
+  (e)=>{res.status(400).send('error:',e)}
+);
 });
 
 app.listen(3000,
