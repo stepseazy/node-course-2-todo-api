98e60bd65d01fe0623310834b092b4d103febfe5
Rik Brutsaert <hb31@cornell.edu>
2018-01-06 14:32:27 -0800

    set up test and dev environments

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,6 @@
 var mongoose=require('mongoose');
 
 mongoose.Promise=global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
-
-module.exports={
-  mongoose
-};
+mongoose.connect(process.env.MONGODB_URI);
+console.log(process.env.MONGODB_URI);
+module.exports={mongoose};
