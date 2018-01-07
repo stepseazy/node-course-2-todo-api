35d055348d4713a19cc684b95903077d2b33598f
Rik Brutsaert <hb31@cornell.edu>
2018-01-06 21:15:59 -0800

    add /post users route and token methods

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,5 +2,5 @@ var mongoose=require('mongoose');
 
 mongoose.Promise=global.Promise;
 mongoose.connect(process.env.MONGODB_URI);
-console.log(process.env.MONGODB_URI);
+
 module.exports={mongoose};
--- server/server.js
+++ server/server.js
@@ -97,6 +97,21 @@ app.patch('/todos/:id',(req,res)=>{
     res.status(400).send();
   });
 });
+//POST /users
+app.post('/users',(req,res)=>{
+    var body=_.pick(req.body,['email','password']);
+    var user=new User(body);
+    user.save().then(
+      ()=>{
+        return user.generateAuthToken();
+        // res.send(r);
+      })
+      .then((token)=>{
+        res.header('x-auth', token).send(user);
+      })
+      .catch((e)=>{res.status(400).send(e);});
+  }
+);
 
 app.listen(port,
   ()=>{
