ec91366897079da9fbbbb36f7f8ae6900ab2cd12
Rik Brutsaert <hb31@cornell.edu>
2018-03-15 22:13:02 -0700

    add tests for post/users lgin

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,6 +1,7 @@
 const request=require('supertest');
 const expect=require('expect');
 const {ObjectID}=require('mongodb');
+const _=require('lodash');
 
 const {app}=require('./../server');
 const {Todo}=require('./../models/todo');
@@ -202,7 +203,7 @@ describe('POST /users', ()=>{
         expect(user.password).not.toBe(password);
         done();
 
-      });
+      }).catch((e)=>done(e));
     });
   });
   it('should return validation errors if request invalid', (done)=>{
@@ -222,6 +223,55 @@ describe('POST /users', ()=>{
     .send({email,password})
     .expect(400)
     .end(done);
+  });
+
+});
+
+describe('POST  /users/login',()=>{
+  it('should login user and return auth token',(done)=>{
+    request (app)
+    .post('/users/login')
+    .send({
+      email:users[1].email,
+      password:users[1].password
+    })
+    .expect(200)
+    .expect((res)=>{
+      expect(res.headers['x-auth']).toBeTruthy();
+    })
+    .end((err,res)=>{
+      if(err){
+        return done(err);
+      }
+      User.findById(users[1]._id).then((user)=>{
+        expect(user.tokens[0].access).toMatch('auth');
+        expect(user.tokens[0].token).toMatch(res.headers['x-auth']);
+
+        done();
+      }).catch((e)=>done(e));
+    });
+});
+  it('should reject invalid login',(done)=>{
+    request (app)
+    .post('/users/login')
+    .send({
+      email:users[1].email,
+      password:users[0].password
+    })
+    .expect(400)
+    .expect((res)=>{
+      expect(res.headers['x-auth']).toBeUndefined();
+    })
+    .end((err,res)=>{
+      if(err){
+        return done(err);
+      }
+      User.findById(users[1]._id).then((user)=>{
+        expect(user.tokens.length).toBe(0);
+
+        done();
+      }).catch((e)=>done(e));
+    });
   });
 
 });
