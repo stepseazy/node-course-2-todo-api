f3f616d4ec59a31ec0c5289947ea09175d53a01a
Rik Brutsaert <hb31@cornell.edu>
2018-07-25 22:31:40 -0700

    update config to use json file

--- server/models/user.js
+++ server/models/user.js
@@ -40,7 +40,7 @@ var UserSchema=new mongoose.Schema({
   UserSchema.methods.generateAuthToken=function () {
     var user=this;
     var access='auth';
-    var token=jwt.sign({_id:user._id.toHexString(), access},'abc123').toString();
+    var token=jwt.sign({_id:user._id.toHexString(), access},process.env.JWT_SECRET).toString();
 
     user.tokens.push({access, token});
     return user.save().then(()=>{
@@ -60,7 +60,7 @@ var UserSchema=new mongoose.Schema({
     var User=this;
     var decoded;
     try{
-      decoded=jwt.verify(token,'abc123');
+      decoded=jwt.verify(token,process.env.JWT_SECRET);
     }catch(e){
         return Promise.reject();
     }
